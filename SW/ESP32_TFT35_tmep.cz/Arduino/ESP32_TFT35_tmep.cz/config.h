533f288b3179357dcbcea22dbaff896078e03afc
Jarda Burian <jarda-burian@hotmail.cz>
2022-11-21 10:29:16 +0100

    Add Arduino examples of tmep.cz and demo

--- SW/ESP32_TFT35_tmep.cz/Arduino/ESP32_TFT35_tmep.cz/config.h
+++ SW/ESP32_TFT35_tmep.cz/Arduino/ESP32_TFT35_tmep.cz/config.h
@@ -1,8 +1,5 @@
-//char ssid[] = "yourWiFiSSIDname";   // your network SSID (name)
-//char pass[] = "yourWiFipassword";   // your network password
-
-char ssid[] = "WiFi_7"; // your network SSID (name)
-char pass[] = "WiFiBurian7"; // your network password
+char ssid[] = "yourWiFiSSIDname";   // your network SSID (name)
+char pass[] = "yourWiFipassword";   // your network password
 
 //You can try with our test Weather Station first 
 //LasKKit Test Weather Station, Rychnov nad Kneznou
