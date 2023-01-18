bb796fa13c25dbc8b946a62a25e24d4e1ec56b9f
Konstantin Láska <konstantin.laska@icloud.com>
2023-01-18 10:54:04 +0100

    folder changes

---
+++ SW/Setup300_ILI9488_ESPD-3_5.h
@@ -0,0 +1,42 @@
+// Konfigurace pro LaskaKit ESPD-3,5"
+// Nakopirovat do slozky Arduino\libraries\TFT_eSPI\User_Setups
+// Do souboru Arduino\libraries\TFT_eSPI\User_Setup_Select.h pridat radek: #include <User_Setups/Setup300_ILI9488_ESPD-3_5.h> do sekce #ifndef USER_SETUP_LOADED
+#define USER_SETUP_ID 300
+
+#define ILI9488_DRIVER
+
+ #define TFT_BL   33            // LED back-light control pin
+ #define TFT_BACKLIGHT_ON HIGH  // Level to turn ON back-light (HIGH or LOW)
+
+#define TFT_MISO 12
+#define TFT_MOSI 13
+#define TFT_SCLK 14
+#define TFT_CS   15  // Chip select control pin
+#define TFT_DC   32  // Data Command control pin
+#define TFT_RST  -1
+
+
+#define LOAD_GLCD   // Font 1. Original Adafruit 8 pixel font needs ~1820 bytes in FLASH
+#define LOAD_FONT2  // Font 2. Small 16 pixel high font, needs ~3534 bytes in FLASH, 96 characters
+#define LOAD_FONT4  // Font 4. Medium 26 pixel high font, needs ~5848 bytes in FLASH, 96 characters
+#define LOAD_FONT6  // Font 6. Large 48 pixel font, needs ~2666 bytes in FLASH, only characters 1234567890:-.apm
+#define LOAD_FONT7  // Font 7. 7 segment 48 pixel font, needs ~2438 bytes in FLASH, only characters 1234567890:.
+#define LOAD_FONT8  // Font 8. Large 75 pixel font needs ~3256 bytes in FLASH, only characters 1234567890:-.
+#define LOAD_GFXFF  // FreeFonts. Include access to the 48 Adafruit_GFX free fonts FF1 to FF48 and custom fonts
+
+#define SMOOTH_FONT
+
+
+// #define SPI_FREQUENCY   1000000
+// #define SPI_FREQUENCY   5000000
+// #define SPI_FREQUENCY  10000000
+// #define SPI_FREQUENCY  20000000
+// #define SPI_FREQUENCY  27000000
+// #define SPI_FREQUENCY  40000000
+ #define SPI_FREQUENCY  55000000 // for v2 only
+// #define SPI_FREQUENCY  80000000
+#define SPI_READ_FREQUENCY  20000000
+#define SPI_TOUCH_FREQUENCY  2500000
+
+
+// #define SUPPORT_TRANSACTIONS
