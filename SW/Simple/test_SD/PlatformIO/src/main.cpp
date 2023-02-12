e6b34cc3cc04cfe376c242538b0dfc15f15f9b10
Jarda Burian <jarda-burian@hotmail.cz>
2023-02-12 23:22:02 +0100

    Update test_SD to be visible on display

--- SW/Simple/test_SD/PlatformIO/src/main.cpp
+++ SW/Simple/test_SD/PlatformIO/src/main.cpp
@@ -14,220 +14,203 @@
  *    D0       MISO
  *    D1       -
  */
+#include <TFT_eSPI.h> // Hardware-specific library
+#include <FS.h>
+#include <SD.h>
+#include <SPI.h>
 #include <Arduino.h>
-#include "FS.h"
-#include "SD.h"
-#include "SPI.h"
 
-#define SD_CS_PIN       4
+// TFT SPI
+#define TFT_LED 33			// TFT backlight pin
+#define TFT_LED_PWM 100 // dutyCycle 0-255 last minimum was 15
+#define TFT_DISPLAY_RESOLUTION_X 480
+#define TFT_DISPLAY_RESOLUTION_Y 320
+#define CENTRE_X TFT_DISPLAY_RESOLUTION_X / 2
+#define TFT_GREY 0x7BEF
+#define RECT_SIZE_X 100
+#define RECT_SIZE_Y 70
+#define SDTEST_TEXT_PADDING	30
+// Delay between demo pages
+#define WAIT 1000 // Delay between screen tests, set to 0 to demo speed, 2000 to see what it does!
 
-SPIClass SDSPI(HSPI);       // Set SD SPI to HSPI
+#define SD_CS_PIN 4
 
+TFT_eSPI tft = TFT_eSPI(); // Invoke custom library with default width and height
 
-void listDir(fs::FS &fs, const char * dirname, uint8_t levels){
-    Serial.printf("Listing directory: %s\n", dirname);
-
-    File root = fs.open(dirname);
-    if(!root){
-        Serial.println("Failed to open directory");
-        return;
-    }
-    if(!root.isDirectory()){
-        Serial.println("Not a directory");
-        return;
-    }
-
-    File file = root.openNextFile();
-    while(file){
-        if(file.isDirectory()){
-            Serial.print("  DIR : ");
-            Serial.println(file.name());
-            if(levels){
-                listDir(fs, file.path(), levels -1);
-            }
-        } else {
-            Serial.print("  FILE: ");
-            Serial.print(file.name());
-            Serial.print("  SIZE: ");
-            Serial.println(file.size());
-        }
-        file = root.openNextFile();
-    }
+void displayInit()
+{
+	tft.init();
+	tft.setRotation(1);
+	tft.fillScreen(TFT_BLACK);
 }
 
-void createDir(fs::FS &fs, const char * path){
-    Serial.printf("Creating Dir: %s\n", path);
-    if(fs.mkdir(path)){
-        Serial.println("Dir created");
-    } else {
-        Serial.println("mkdir failed");
-    }
+int appendFile(fs::FS &fs, const char *path, const char *message)
+{
+	File file = fs.open(path, FILE_APPEND);
+	if (!file)
+	{
+		return -1;
+	}
+	if (file.print(message))
+	{
+		file.close();
+	}
+	else
+	{
+		file.close();
+		return -2;
+	}
+	return 0;
 }
 
-void removeDir(fs::FS &fs, const char * path){
-    Serial.printf("Removing Dir: %s\n", path);
-    if(fs.rmdir(path)){
-        Serial.println("Dir removed");
-    } else {
-        Serial.println("rmdir failed");
-    }
+int writeFile(fs::FS &fs, const char *path, const char *message)
+{
+	File file = fs.open(path, FILE_WRITE);
+	if (!file)
+	{
+		return -1;
+	}
+	if (file.print(message))
+	{
+		file.close();
+	}
+	else
+	{
+		file.close();
+		return -2;
+	}
+	return 0;
 }
 
-void readFile(fs::FS &fs, const char * path){
-    Serial.printf("Reading file: %s\n", path);
-
-    File file = fs.open(path);
-    if(!file){
-        Serial.println("Failed to open file for reading");
-        return;
-    }
-
-    Serial.print("Read from file: ");
-    while(file.available()){
-        Serial.write(file.read());
-    }
-    file.close();
+int readFile(fs::FS &fs, const char *path)
+{
+	uint16_t lines = 0;
+	File file = fs.open(path);
+	if (!file)
+	{
+		return -1;
+	}
+	while (file.available())
+	{
+		if (file.read() == '\n')
+		{
+			lines++;
+		}
+	}
+	file.close();
+	return lines;
 }
 
-void writeFile(fs::FS &fs, const char * path, const char * message){
-    Serial.printf("Writing file: %s\n", path);
-
-    File file = fs.open(path, FILE_WRITE);
-    if(!file){
-        Serial.println("Failed to open file for writing");
-        return;
-    }
-    if(file.print(message)){
-        Serial.println("File written");
-    } else {
-        Serial.println("Write failed");
-    }
-    file.close();
+void printLastLine(int32_t x, int32_t y, fs::FS &fs, const char *path)
+{
+	int lines = readFile(fs, path);
+	int actLines = 1;
+	char text[10] = {0};
+	File file = fs.open(path);
+	if (!file)
+	{
+		tft.drawString("Unable to open file", x, y);
+		return;
+	}
+	while (actLines < lines)
+	{
+		char t = file.read();
+		if (t == '\n')
+		{
+			actLines++;
+		}
+	}
+	int i = 0;
+	while (file.available())
+	{
+		char t = file.read();
+		text[i++] = t;
+	}
+	file.close();
+	tft.drawString("Last line written: " + String(text), x, y);
 }
 
-void appendFile(fs::FS &fs, const char * path, const char * message){
-    Serial.printf("Appending to file: %s\n", path);
-
-    File file = fs.open(path, FILE_APPEND);
-    if(!file){
-        Serial.println("Failed to open file for appending");
-        return;
-    }
-    if(file.print(message)){
-        Serial.println("Message appended");
-    } else {
-        Serial.println("Append failed");
-    }
-    file.close();
+int SDtestInit(int32_t x, int32_t y)
+{
+	uint8_t cardType;
+	uint64_t cardSize;
+	if (!SD.begin(SD_CS_PIN))
+	{
+		return -1;
+	}
+	cardType = SD.cardType();
+	if (cardType == CARD_NONE)
+	{
+		return -1;
+	}
+	cardSize = SD.cardSize() / (1024 * 1024);
+	if (cardType == CARD_MMC)
+	{
+		tft.drawString("SD Card Type: MMC, size: " + String(cardSize) + "Mb", x, y);
+	}
+	else if (cardType == CARD_SD)
+	{
+		tft.drawString("SD Card Type: SDSC, size: " + String(cardSize) + "Mb", x, y);
+	}
+	else if (cardType == CARD_SDHC)
+	{
+		tft.drawString("SD Card Type: SDHC, size: " + String(cardSize) + "Mb", x, y);
+	}
+	else
+	{
+		tft.drawString("SD Card Type: UNKNOWN, size: " + String(cardSize) + "Mb", x, y);
+	}
+	return 0;
 }
 
-void renameFile(fs::FS &fs, const char * path1, const char * path2){
-    Serial.printf("Renaming file %s to %s\n", path1, path2);
-    if (fs.rename(path1, path2)) {
-        Serial.println("File renamed");
-    } else {
-        Serial.println("Rename failed");
-    }
-}
-
-void deleteFile(fs::FS &fs, const char * path){
-    Serial.printf("Deleting file: %s\n", path);
-    if(fs.remove(path)){
-        Serial.println("File deleted");
-    } else {
-        Serial.println("Delete failed");
-    }
-}
-
-void testFileIO(fs::FS &fs, const char * path){
-    File file = fs.open(path);
-    static uint8_t buf[512];
-    size_t len = 0;
-    uint32_t start = millis();
-    uint32_t end = start;
-    if(file){
-        len = file.size();
-        size_t flen = len;
-        start = millis();
-        while(len){
-            size_t toRead = len;
-            if(toRead > 512){
-                toRead = 512;
-            }
-            file.read(buf, toRead);
-            len -= toRead;
-        }
-        end = millis() - start;
-        Serial.printf("%u bytes read for %u ms\n", flen, end);
-        file.close();
-    } else {
-        Serial.println("Failed to open file for reading");
-    }
-
-
-    file = fs.open(path, FILE_WRITE);
-    if(!file){
-        Serial.println("Failed to open file for writing");
-        return;
-    }
-
-    size_t i;
-    start = millis();
-    for(i=0; i<2048; i++){
-        file.write(buf, 512);
-    }
-    end = millis() - start;
-    Serial.printf("%u bytes written for %u ms\n", 2048 * 512, end);
-    file.close();
+void SDtest()
+{
+	tft.fillScreen(TFT_BLACK);
+	tft.setTextSize(1);
+	tft.setTextFont(4);
+	tft.setTextColor(TFT_WHITE, TFT_BLACK);
+	tft.setTextDatum(BC_DATUM);
+	if (SDtestInit(TFT_DISPLAY_RESOLUTION_X / 2, SDTEST_TEXT_PADDING))
+	{
+		tft.drawString("SD card not found", TFT_DISPLAY_RESOLUTION_X / 2, SDTEST_TEXT_PADDING);
+		SD.end();
+		return;
+	}
+	tft.drawString("SD card mounted", TFT_DISPLAY_RESOLUTION_X / 2, SDTEST_TEXT_PADDING * 2);
+	File file = SD.open("/test.txt", FILE_APPEND);
+	if (!file)
+	{
+		if (writeFile(SD, "/test.txt", "test\n"))
+		{
+			tft.drawString("Unable to write into file", TFT_DISPLAY_RESOLUTION_X / 2, SDTEST_TEXT_PADDING * 3);
+			return;
+		}
+		tft.drawString("File test.txt created, test line written", TFT_DISPLAY_RESOLUTION_X / 2, SDTEST_TEXT_PADDING * 3);
+		tft.drawString("Number of lines in the document: " + String(readFile(SD, "/test.txt")), TFT_DISPLAY_RESOLUTION_X / 2, SDTEST_TEXT_PADDING * 4);
+		printLastLine(TFT_DISPLAY_RESOLUTION_X / 2, SDTEST_TEXT_PADDING * 5, SD, "/test.txt");
+	}
+	else
+	{
+		tft.drawString("Number of lines in the document: " + String(readFile(SD, "/test.txt")), TFT_DISPLAY_RESOLUTION_X / 2, SDTEST_TEXT_PADDING * 3);
+		if (appendFile(SD, "/test.txt", "test\n"))
+		{
+			tft.drawString("Unable to append into test.txt", TFT_DISPLAY_RESOLUTION_X / 2, SDTEST_TEXT_PADDING * 4);
+			return;
+		}
+		tft.drawString("Test line appended to test.txt", TFT_DISPLAY_RESOLUTION_X / 2, SDTEST_TEXT_PADDING * 4);
+		tft.drawString("Number of lines in the document: " + String(readFile(SD, "/test.txt")), TFT_DISPLAY_RESOLUTION_X / 2, SDTEST_TEXT_PADDING * 5);
+		printLastLine(TFT_DISPLAY_RESOLUTION_X / 2, SDTEST_TEXT_PADDING * 6, SD, "/test.txt");
+	}
+	SD.end();
 }
 
 void setup(){
-    pinMode(15, OUTPUT);
-    digitalWrite(15, HIGH);             // Setting CS pin of display high
-    pinMode(SD_CS_PIN, OUTPUT);
-    digitalWrite(SD_CS_PIN, HIGH);      // Setting CS pin high
-    Serial.begin(115200);
-    if(!SD.begin(SD_CS_PIN, SDSPI)){
-        Serial.println("Card Mount Failed");
-        return;
-    }
-    uint8_t cardType = SD.cardType();
-    
-    if(cardType == CARD_NONE){
-        Serial.println("No SD card attached");
-        return;
-    }
-
-    Serial.print("SD Card Type: ");
-    if(cardType == CARD_MMC){
-        Serial.println("MMC");
-    } else if(cardType == CARD_SD){
-        Serial.println("SDSC");
-    } else if(cardType == CARD_SDHC){
-        Serial.println("SDHC");
-    } else {
-        Serial.println("UNKNOWN");
-    }
-
-    uint64_t cardSize = SD.cardSize() / (1024 * 1024);
-    Serial.printf("SD Card Size: %lluMB\n", cardSize);
-
-    listDir(SD, "/", 0);
-    createDir(SD, "/mydir");
-    listDir(SD, "/", 0);
-    removeDir(SD, "/mydir");
-    listDir(SD, "/", 2);
-    writeFile(SD, "/hello.txt", "Hello ");
-    appendFile(SD, "/hello.txt", "World!\n");
-    readFile(SD, "/hello.txt");
-    deleteFile(SD, "/foo.txt");
-    renameFile(SD, "/hello.txt", "/foo.txt");
-    readFile(SD, "/foo.txt");
-    testFileIO(SD, "/test.txt");
-    Serial.printf("Total space: %lluMB\n", SD.totalBytes() / (1024 * 1024));
-    Serial.printf("Used space: %lluMB\n", SD.usedBytes() / (1024 * 1024));
+    ledcSetup(1, 5000, 8);		 // ledChannel, freq, resolution
+	ledcAttachPin(TFT_LED, 1); // ledPin, ledChannel
+	ledcWrite(1, TFT_LED_PWM); // dutyCycle 0-255
+    displayInit();
+    SDtest();
 }
 
 void loop(){
-
 }
