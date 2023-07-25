adad277271acc1b8d1b7318022bda0f13d4b8980
Jarda <jarda-burian@hotmail.cz>
2023-07-26 00:12:20 +0200

    Delete platformio files

--- SW/ESP32_TFT35_tmep.cz/PlatformIO/src/config.h
+++
@@ -1,9 +0,0 @@
-char ssid[] = "yourWiFiSSIDname";   // your network SSID (name)
-char pass[] = "yourWiFipassword";   // your network password
-
-//You can try with our test Weather Station first 
-//LasKKit Test Weather Station, Rychnov nad Kneznou
-const char* jsonurl = "https://tmep.cz/vystup-json.php?id=4551&export_key=bx9hd5jq8g&extended=1";
-
-//unsigned long myChannelNumber = 668454;  // change to your channel number
-//const char * myReadAPIKey = "XSO3DDPYSQWGYCC4";  //change to your Thingspeak channel read api key
