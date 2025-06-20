9a593357ed3d70b1a7e67943a4973eb4a32c42db
Konstantin Láska <konstantin.laska@icloud.com>
2025-06-20 21:57:37 +0200

    updated for new board v3 and renamed

---
+++ SW/Simple/text_and_bitmap/text_and_bitmap.ino
@@ -0,0 +1,74 @@
+/* 
+ * Basic test with text and bitmap in internal memory for LaskaKit ESPD-3.5" 320x480, ILI9488 
+ * example from TFT_eSPI library is used
+ * 
+ * How to steps:
+ * 1. Copy file from https://github.com/LaskaKit/ESPD-35/tree/main/SW to Arduino/libraries/TFT_eSPI/User_Setups/
+ *    - for version v2.3 and before:  Setup300_ILI9488_ESPD-3_5_v2.h 
+ *    - for version v3 and above:     Setup303_ILI9488_ESPD-3_5_v3.h
+ * 2. in Arduino/libraries/TFT_eSPI/User_Setup_Select.h 
+      a. comment: #include <User_Setup.h> 
+      b. add: 
+          - for version v2.3 and before:  #include <User_Setups/Setup300_ILI9488_ESPD-3_5_v2.h>  // Setup file for LaskaKit ESPD-3.5" 320x480, ILI9488 
+          - for version v3 and above:     #include <User_Setups/Setup303_ILI9488_ESPD-3_5_v3.h>  // Setup file for LaskaKit ESPD-3.5" 320x480, ILI9488 V3
+ * 
+ * Board constants:
+      TFT_BL          - LED back-light use: analogWrite(TFT_BL, TFT_BL_PWM);
+      POWER_OFF_PIN   - Pull LOW to switch board off
+      TOUCH_INT       - Touch interrupt pin
+    * I2C (µŠup and devices (only from v3)):
+      I2C_SDA         - Data pin 
+      I2C_SCL         - Clock pin
+    * SPI (µŠup (only from v3) and SD card):
+      SPI_MISO        - MISO pin
+      SPI_MOSI        - MOSI pin
+      SPI_SCK         - Clock pin
+      SPI_USUP_CS     - µŠup Chip Select pin (only from v3)
+      SPI_SD_CS       - SD Card Chip Select pin
+    * I2S (only from v3):
+      I2S_LRC         - Word select a.k.a. left-right clock pin
+      I2S_DOUT        - Serial data pin
+      I2S_BCLK        - Serial clock a.k.a. bit clock pin
+    * Battery mesurement:
+      BAT_PIN         - Battery voltage mesurement
+      deviderRatio    - Voltage devider ratio on ADC pin 1MOhm + 1.3MOhm
+ *
+ * Email:podpora@laskakit.cz
+ * Web:laskakit.cz
+*/
+
+#include <TFT_eSPI.h> // Hardware-specific library
+#include <SPI.h>
+#include "bitmaps.h"
+#include "OpenSansSB_40px.h"
+
+#define TFT_BL_PWM 255 // Backlight brightness 0-255
+
+TFT_eSPI tft = TFT_eSPI();
+
+void setup()
+{
+  tft.init();
+  tft.setRotation(1);
+  
+  analogWrite(TFT_BL, TFT_BL_PWM);      // Set brightness of backlight
+}
+void loop()
+{ 
+    tft.fillScreen(TFT_WHITE);
+    tft.setTextColor(TFT_BLACK, TFT_WHITE);
+   
+    /*
+    * LCD image convertor 
+    * Block size: 16 bit
+    * Byte order: Big Endian
+    * Rest: default settings
+    */
+    tft.pushImage(40, 100, 400, 102, laskakit);
+    
+    tft.setCursor(100,50);
+    tft.setFreeFont(&OpenSansSB_40px); 
+    tft.print("Bastliri bastli :-) ");
+
+    delay(10000);
+}
